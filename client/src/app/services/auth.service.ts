2bf5d08c84e42563cad6ca07b5ee2246e7bf8b0d
FranOrteg <ortegfran@gmail.com>
2025-06-03 10:56:56 +0200

    Init Client

---
+++ client/src/app/services/auth.service.ts
@@ -0,0 +1,48 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+interface UserRegister {
+  name: string;
+  email: string;
+  password: string;
+  role: 'Administrador' | 'Profesor' | 'Alumno';
+}
+
+interface UserLogin {
+  email: string;
+  password: string;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  private baseUrl = 'http://localhost:3000/api/user';
+
+  constructor(private http: HttpClient) {}
+
+  register(user: UserRegister): Observable<any> {
+    return this.http.post(`${this.baseUrl}/register`, user);
+  }
+
+  login(credentials: UserLogin): Observable<any> {
+    return this.http.post(`${this.baseUrl}/login`, credentials);
+  }
+
+  saveToken(token: string): void {
+    localStorage.setItem('token', token);
+  }
+
+  getToken(): string | null {
+    return localStorage.getItem('token');
+  }
+
+  logout(): void {
+    localStorage.removeItem('token');
+  }
+
+  isLoggedIn(): boolean {
+    return !!localStorage.getItem('token');
+  }
+} 
