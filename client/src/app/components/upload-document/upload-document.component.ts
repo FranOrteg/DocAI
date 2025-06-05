4e12a3fb86f2c26ecf82dea08f3529cda6b5e1f1
FranOrteg <ortegfran@gmail.com>
2025-06-05 12:10:46 +0200

    Upload Document Input reset when upload

--- client/src/app/components/upload-document/upload-document.component.ts
+++ client/src/app/components/upload-document/upload-document.component.ts
@@ -1,4 +1,4 @@
-import { Component, Output, EventEmitter, Input, ViewChild } from '@angular/core';
+import { Component, Output, EventEmitter, Input, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
 import { FormGroup, FormControl, Validators, ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { DocumentService } from '../../services/document.service';
@@ -11,16 +11,21 @@ import { DocumentListComponent } from '../document-list/document-list.component'
   templateUrl: './upload-document.component.html',
   styleUrls: ['./upload-document.component.css']
 })
-export class UploadDocumentComponent {
+export class UploadDocumentComponent implements AfterViewInit{
   @Input() courseId!: number;
   @Output() uploadSuccess = new EventEmitter<void>();
   @ViewChild('docList') docListComponent!: DocumentListComponent;
+  @ViewChild('fileInput', { static: false }) fileInput!: ElementRef<HTMLInputElement>;
 
   uploadForm = new FormGroup({
     document: new FormControl<File | null>(null, Validators.required)
   });
 
   constructor(private documentService: DocumentService) {}
+
+  ngAfterViewInit() {
+    console.log('✅ fileInput está definido:', this.fileInput);
+  }  
 
   onFileChange(event: Event) {
     const file = (event.target as HTMLInputElement).files?.[0] || null;
@@ -29,19 +34,27 @@ export class UploadDocumentComponent {
 
   async onSubmit() {
     if (this.uploadForm.invalid || !this.courseId) return;
-
+  
     const file = this.uploadForm.value.document!;
-
     try {
       await this.documentService.uploadDocument(this.courseId, file);
       alert('✅ Documento subido correctamente');
+  
       this.uploadForm.reset();
+  
+      if (this.fileInput) {
+        this.fileInput.nativeElement.value = '';
+      }
+  
+      console.log('Input file actual:', this.fileInput?.nativeElement?.value);
+
       this.uploadSuccess.emit();
     } catch (err) {
       console.error('❌ Error al subir documento:', err);
       alert('Error al subir documento');
     }
   }
+  
 
   onDocumentUploaded() {
     this.docListComponent.refresh();
