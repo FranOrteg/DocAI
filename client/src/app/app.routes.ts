552672843d532f978409caa0f893577df7150e83
FranOrteg <ortegfran@gmail.com>
2025-06-07 21:59:21 +0200

    Home

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -2,13 +2,15 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 import { roleGuard } from './guards/role.guard';
+import { HomeComponent } from './components/home/home.component';
 
 
 export const routes: Routes = [
 
     { path: '', redirectTo: 'login', pathMatch: 'full' },
-    { path: 'register', component: RegisterComponent },
+    { path: 'home', component:HomeComponent},
     { path: 'login', component: LoginComponent },
+    { path: 'register', component: RegisterComponent },
 
     {
         path: 'admin',
